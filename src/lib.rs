c56f0cb9a633ed9847238a8d7d9711e132d9b9fe
Abhinandh S <abhinandhsuby@proton.me>
2026-02-01 17:47:34 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,18 @@
-use route::Route;
+use wasm_bindgen::prelude::*;
 use yew::prelude::*;
-use yew_router::prelude::*;
-
-use self::pages::articles::Article;
-
-mod utils;
-mod components;
-mod pages;
-mod route;
-
-fn main() {
-    yew::Renderer::<App>::new().render();
-}
 
 #[function_component(App)]
 fn app() -> Html {
-    html!(
-        <BrowserRouter> // `HashRouter` is needed for github pages
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
-    )
+    html! {
+        <div>
+            <h1>{"Hello from Yew SSR!"}</h1>
+            <p style="color: blue;">{"This was rendered on Deno Deploy"}</p>
+        </div>
+    }
 }
 
-fn switch(routes: Route) -> Html {
-    match routes {
-        Route::Home => html! { <pages::home::HomePage /> },
-        Route::Portfolio => html! { <pages::portfolio::Portfolio /> },
-        Route::Articles { id } => html! { <Article post_id={id} /> },
-        Route::ArticlesRoute => html! { <pages::articles::ArticleIndex /> },
-        Route::About => html! { <pages::about::About /> },
-        Route::NotFound => html! { <pages::_404::NotFound /> },
-    }
+#[wasm_bindgen]
+pub async fn render() -> String {
+    let renderer = yew::ServerRenderer::<App>::new();
+    renderer.render().await
 }
